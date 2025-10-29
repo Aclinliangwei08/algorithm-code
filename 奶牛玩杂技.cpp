8acf75941ddf96dd196918770e4e62ef2e94f23b
shanfeng <3407395319@student.gdufe.edu.cn>
2025-10-29 11:49:17 +0800

    main

--- 奶牛玩杂技.cpp
+++ 奶牛玩杂技.cpp
@@ -1,30 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int N=5e4+10;
 typedef long long LL;
-struct ndd {
-    LL w;
-    LL s;
-} arr[N];
-bool cmp(ndd a,ndd b)
-{
-    return a.w+a.s<b.w+b.s; //贪心策略，体重+力量越大，越待在下面
+const int N=5e4+10;
+struct ndd{
+    int w;
+    int s;
+}a[N];
+bool cmp(ndd &x,ndd &y){
+    return x.w+x.s<y.w+y.s;
 }
 int main()
 {
     int n;
-    scanf("%d",&n);
-    for(int i=0;i<n;i++)
+    cin>>n;
+    for(int i=1;i<=n;i++)
     {
-        scanf("%lld %lld",&arr[i].w,&arr[i].s);
+        cin>>a[i].w>>a[i].s;
     }
-    sort(arr,arr+n,cmp);
-    LL weight=0;
-    LL ans=LLONG_MIN;
-    for(int i=0;i<n;i++)//从上到下遍历，找出答案
+    sort(a+1,a+n+1,cmp);
+    LL w=0,ans=-1e9-10;
+    for(int i=1;i<=n;i++)
     {
-        ans=max(ans,weight-arr[i].s);
-        weight+=arr[i].w;
+        ans=max(ans,w-a[i].s);
+        w+=a[i].w;
     }
     cout<<ans<<endl;
     return 0;
